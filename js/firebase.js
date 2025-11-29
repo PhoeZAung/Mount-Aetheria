eea061db360a1effc9d3bee295914b924f7bca2f
PhoeZAung <phoe.z.aung@gmail.com>
2025-11-28 23:03:30 -0700

    1.1b
    first test before live, added 4 new tab

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBpoi5qvqUehnhcyhfB7d0gtelNUICIthw",
+  authDomain: "mount-aetheria.firebaseapp.com",
+  projectId: "mount-aetheria",
+  storageBucket: "mount-aetheria.firebasestorage.app",
+  messagingSenderId: "492855693446",
+  appId: "1:492855693446:web:f9948569254a7220c38107",
+  measurementId: "G-0PF84KDN7L"
+};
