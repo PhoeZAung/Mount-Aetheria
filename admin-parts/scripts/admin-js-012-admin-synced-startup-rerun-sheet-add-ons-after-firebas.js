7ca1236585d0cf130893724cfe1543efa51e1db8
PhoeZAung <phoe.z.aung@gmail.com>
2026-07-21 05:13:44 -0600

    Delete admin-parts directory

--- admin-parts/scripts/admin-js-012-admin-synced-startup-rerun-sheet-add-ons-after-firebas.js
+++
@@ -1,9 +0,0 @@
-
-
-/* --- Admin synced startup: rerun sheet add-ons after Firebase/admin module becomes available --- */
-(function(){
-  function rerun(){
-    try { window.maFfd20RefreshOptions?.(); } catch(e) {}try { window.maV34RefreshWeaponProficiency?.(); } catch(e) {}}
-  window.addEventListener('load', () => { setTimeout(rerun, 300); setTimeout(rerun, 1200); setTimeout(rerun, 2500); });
-})();
-
